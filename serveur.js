a057a70e559f13ecb5e6ce866dca492c5298acd6
Juliette Grandin <juliette.grandin@gmail.com>
2017-12-07 17:19:39 +0100

    v0.5 bdd

--- serveur.js
+++ serveur.js
@@ -2,6 +2,7 @@ var express = require('express'); // pour importer le module
 var app = express(); // initialisation du serveur
 var bodyParser = require('body-parser');
 var request = require('request');
+var mongoose = require('mongoose');
 app.set('view engine', 'ejs');
 app.use(express.static('public')); //va chercher docs dans "public", ici donc la feuille de style
 app.use(bodyParser.json());
@@ -13,6 +14,22 @@ var upload = multer ({
 
 var carList = [];
 
+var options = { server: { socketOptions: {connectTimeoutMS: 30000 } }};
+mongoose.connect('mongodb://juliettegb:Drivy@ds133496.mlab.com:33496/drivy', options, function(err){
+  console.log(err);
+})
+
+var carSchema = mongoose.Schema({
+  modele: String,
+  marque: String,
+  ville: String,
+  places: Number,
+  lat: Number,
+  lon: Number,
+});
+
+var carModel = mongoose.model('Car', carSchema);
+
 app.get('/', function(req, res){
   res.render('carForm', {carList});
 });
@@ -20,9 +37,30 @@ app.get('/', function(req, res){
 app.post('/form', upload.array(), function(req, res){
   console.log(req.body);
 
-  request("https://maps.googleapis.com/maps/api/geocode/json?address="+req.body.ville+"&key=AIzaSyDLiaDOwBIN3ZMlrVJ3KVSbqqsXbC6SXb4", function(error, response, body){
-    var body = JSON.parse(body);
-    console.log(body);
+  request("https://maps.googleapis.com/maps/api/geocode/json?address="+req.body.ville+"&key=AIzaSyDLiaDOwBIN3ZMlrVJ3KVSbqqsXbC6SXb4", function(error, response, data){
+    var data = JSON.parse(data);
+    console.log(data);
+    var lat = data.results[0].geometry.location.lat;
+    var lon = data.results[0].geometry.location.lng;
+    console.log(lat);
+    console.log(lon);
+    //var newCar = {modele: body.modele, marque: body.marque, ville: body.ville, places: body.places, lat: lat, lon: lon};
+    //carList.push(newCar);
+
+    var car = new carModel({ //ordre d'enregistrement
+      modele: req.body.modele,
+      marque: req.body.marque,
+      ville: req.body.ville,
+      places: req.body.places,
+      lat: lat,
+      lon: lon
+    });
+
+    car.save(function (error, car){ //on est sur de l'asynchrone donc fonction de callback qui sera exécutée lorsque le backend aura fini son boulot!
+      console.log(error);
+      console.log(car);
+    });
+
   });
 
   res.render('carForm', {carList});
