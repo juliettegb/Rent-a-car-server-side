eb9780b35fb79a7c2f7d2744c3308d02b699fcf9
Juliette Grandin <juliette.grandin@gmail.com>
2017-12-07 15:59:01 +0100

    v0.3

--- serveur.js
+++ serveur.js
@@ -1,7 +1,14 @@
 var express = require('express'); // pour importer le module
 var app = express(); // initialisation du serveur
+var bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(express.static('public')); //va chercher docs dans "public", ici donc la feuille de style
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+var multer = require('multer');
+var upload = multer ({
+  dest: './uploads/'
+});
 
 var carList = [];
 
@@ -9,9 +16,9 @@ app.get('/', function(req, res){
   res.render('carForm', {carList});
 });
 
-app.get('/form', function(req, res){
-  console.log(req.query);
-  carList.push(req.query);
+app.post('/form', upload.array(), function(req, res){
+  console.log(req.body);
+  carList.push(req.body);
   res.render('carForm', {carList});
 
 });
