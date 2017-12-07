167bc6a0e8cdae895b09be1c83e56c72cfb459e6
Juliette Grandin <juliette.grandin@gmail.com>
2017-12-07 16:45:46 +0100

    v0.3

--- serveur.js
+++ serveur.js
@@ -1,10 +1,11 @@
 var express = require('express'); // pour importer le module
 var app = express(); // initialisation du serveur
 var bodyParser = require('body-parser');
+var request = require('request');
 app.set('view engine', 'ejs');
 app.use(express.static('public')); //va chercher docs dans "public", ici donc la feuille de style
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 var multer = require('multer');
 var upload = multer ({
   dest: './uploads/'
@@ -18,7 +19,12 @@ app.get('/', function(req, res){
 
 app.post('/form', upload.array(), function(req, res){
   console.log(req.body);
-  carList.push(req.body);
+
+  request("https://maps.googleapis.com/maps/api/geocode/json?address="+req.body.ville+"&key=AIzaSyALD-Dfvhh3rW5r6twUYDyz_T75QMHsDcU", function(error, response, body){
+    var body = JSON.parse(body);
+    console.log(body);
+  });
+
   res.render('carForm', {carList});
 
 });
